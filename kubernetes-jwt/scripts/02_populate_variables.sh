1ebb2b14d86a86d95e0ef2497b2d0b92dc0edbd4
Assaf Hazan <assafjh@gmail.com>
2022-10-24 16:00:16 +0300

    Adding k8s cert authn demo, updating k8s jwt authn

--- kubernetes-jwt/scripts/02_populate_variables.sh
+++ kubernetes-jwt/scripts/02_populate_variables.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-# Populate safe secrets with values
-safe_path=kubernetes/applications/safe/secret
+#==========
+COP_CLI=kubectl
+SAFE_PATH=kubernetes/applications/safe/secret
+#==========
+
+# Checking if a user is logged-in to Conjur-CLI
 conjur whoami
+
+# Populate safe secrets with values
 for i in {1..8}
 do
    if command -p md5sum  >/dev/null 2>&1
     then
-        conjur variable set -i "$safe_path$i" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
+        conjur variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
     else
-        conjur variable set -i "$safe_path$i" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
+        conjur variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
     fi
 done
 
 # Populate authenticator values
 conjur variable set -i conjur/authn-jwt/k8s-cluster1/identity-path -v "/kubernetes/applications"
-conjur variable set -i conjur/authn-jwt/k8s-cluster1/issuer -v "$(echo $(kubectl get --raw /.well-known/openid-configuration | awk -F "," '{print $1}' | tr -d '",' | sed 's#{issuer:##g'))"
+conjur variable set -i conjur/authn-jwt/k8s-cluster1/issuer -v "$(echo $($COP_CLI get --raw /.well-known/openid-configuration | awk -F "," '{print $1}' | tr -d '",' | sed 's#{issuer:##g'))"
 conjur variable set -i conjur/authn-jwt/k8s-cluster1/token-app-property -v "sub"
-conjur variable set -i conjur/authn-jwt/k8s-cluster1/public-keys -v "$(echo '{"type": "jwks","value":'$(kubectl get --raw /openid/v1/jwks)'}')"
+conjur variable set -i conjur/authn-jwt/k8s-cluster1/public-keys -v "$(echo '{"type": "jwks","value":'$($COP_CLI get --raw /openid/v1/jwks)'}')"
