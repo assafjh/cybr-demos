6d0e584734a8e8c8cbad65b9eebcf1d610dbf3f9
Cloud User <ec2-user@ip-172-16-0-128.eu-west-2.compute.internal>
2022-10-25 12:38:25 +0000

    Bug fixes after testing

--- jenkins/deploy_jenkins.sh
+++ jenkins/deploy_jenkins.sh
@@ -5,14 +5,16 @@ CONTAINER_MGR=podman
 CONTAINER_IMG=docker.io/assafhazan/jenkins:conjur
 JENKINS_ADDRESS=
 JENKINS_PORT=8080
+JENKINS_ADMIN_ID=
+JENKINS_ADMIN_PASSWORD=
 #====
 $CONTAINER_MGR container run \
   --name jenkins \
   --detach \
   --privileged \
-  --publish $PORT:8080 \
-  --env JENKINS_ADMIN_ID= \
-  --env JENKINS_ADMIN_PASSWORD= \
+  --publish $JENKINS_PORT:8080 \
+  --env JENKINS_ADMIN_ID="$JENKINS_ADMIN_ID" \
+  --env JENKINS_ADMIN_PASSWORD="$JENKINS_ADMIN_PASSWORD" \
   --env JENKINS_ADDRESS="$JENKINS_ADDRESS" \
   --env JENKINS_PORT="$JENKINS_PORT" \
   $CONTAINER_IMG
