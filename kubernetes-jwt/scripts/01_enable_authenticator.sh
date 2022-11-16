89ac053475935d40e07a30c7325bef7c03eb1f2e
Assaf Hazan <assafjh@gmail.com>
2022-11-16 17:35:22 +0200

    Fix to add authn scripts

--- kubernetes-jwt/scripts/01_enable_authenticator.sh
+++ kubernetes-jwt/scripts/01_enable_authenticator.sh
@@ -5,7 +5,7 @@
 # Is sudo required to run docker/podman - leave empty if no need
 SUDO=
 # Using docker/podman
-CONTAINER_MGR=podman
+CONTAINER_MGR=docker
 # Conjur Leader port
 CONJUR_LEADER_PORT=443
 # Conjur Leader container ID
@@ -16,6 +16,7 @@ AUTHN_TO_ENABLE=authn-jwt/k8s-cluster1
 echo "Enabling $AUTHN_TO_ENABLE authn for Conjur"
 CONJUR_AUTHENTICATORS=$($SUDO $CONTAINER_MGR exec $CONTAINER_ID evoke variable list | grep CONJUR_AUTHENTICATORS)
 CONJUR_AUTHENTICATORS=$(echo $CONJUR_AUTHENTICATORS | awk -F "=" '{print $2}')
+CONJUR_AUTHENTICATORS=$(sed -e 's/^"//' -e 's/"$//' <<< "$CONJUR_AUTHENTICATORS")
 IFS=','
 read -a array <<< "$CONJUR_AUTHENTICATORS"
 if [[ "${IFS}${array[*]}${IFS}" =~ "${IFS}$AUTHN_TO_ENABLE${IFS}" ]]; then
@@ -23,5 +24,6 @@ if [[ "${IFS}${array[*]}${IFS}" =~ "${IFS}$AUTHN_TO_ENABLE${IFS}" ]]; then
     unset IFS
 else
     unset IFS
+    [ -z "$CONJUR_AUTHENTICATORS" ] && CONJUR_AUTHENTICATORS=authn
     $SUDO $CONTAINER_MGR exec -it $CONTAINER_ID evoke variable set CONJUR_AUTHENTICATORS $CONJUR_AUTHENTICATORS,$AUTHN_TO_ENABLE
 fi
