4d08060a2555b67976f35ab0672b431fbf61b6d7
Cloud User <ec2-user@ip-172-16-0-128.eu-west-2.compute.internal>
2022-11-29 13:34:14 +0000

    fixed build package

--- aws-iam-lambda/scripts/01-package-function.sh
+++ aws-iam-lambda/scripts/01-package-function.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-[ -d ../function-source-code/package ] && rm -rf ../function-source-code/package
-[ -f ../function-source-code/conjur-lambda-package.zip ] && rm -rf ../function-source-code/conjur-lambda-package.zip
-pip install -r ../function-source-code/requirements.txt --target ../function-source-code/package
-cd ../function-source-code/package || exit
-zip -r ../conjur-lambda-package.zip .
-cd ..
-zip conjur-lambda-package.zip lambda_function.py conjur_iam_client.py
+cd ../function-source-code
+docker run -it --rm -v "$PWD":/var/task lambci/lambda:build-python3.8 /bin/bash -c /var/task/entrypoint.sh
