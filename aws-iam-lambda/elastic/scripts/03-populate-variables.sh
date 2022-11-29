b336e2db52eb671cf7abd9883f89435c97326402
Cloud User <ec2-user@ip-172-16-0-128.eu-west-2.compute.internal>
2022-11-29 14:44:06 +0000

    refactor

---
+++ aws-iam-lambda/elastic/scripts/03-populate-variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#============ Variables ===============
+# Path to our safe at Conjur, leave as is
+SAFE_PATH=aws/elastic/safe/secret
+#============ Script ===============
+
+# Checking if a user is logged-in to Conjur-CLI
+conjur whoami
+
+# Populate safe secrets with values
+for i in {1..3}
+do
+   if command -p md5sum  /dev/null >/dev/null 2>&1
+    then
+        conjur variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
+    else
+        conjur variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
+    fi
+done
