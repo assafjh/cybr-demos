0c46b8baf07e0668d7ee0ba73dd06b47b549b3da
Assaf Hazan <assafjh@gmail.com>
2022-11-29 15:08:10 +0000

    Updated conjur_iam_client to match community published file, updated packaging to use official amazon image

--- aws-iam/lambda/function-source-code/entrypoint.sh
+++ aws-iam/lambda/function-source-code/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 [ -f ./conjur-lambda-package.zip ] && rm -rf ./conjur-lambda-package.zip
+yum install zip -y
 pip3 install -r requirements.txt --target ./package
 cd ./package || exit
 zip -r ../conjur-lambda-package.zip .
--- aws-iam/lambda/scripts/01-package-function.sh
+++ aws-iam/lambda/scripts/01-package-function.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../function-source-code
-docker run -it --rm -v "$PWD":/var/task lambci/lambda:build-python3.8 /bin/bash -c /var/task/entrypoint.sh
+docker run -it --rm -v "$PWD":/var/task --entrypoint /bin/bash amazon/aws-lambda-python:latest /var/task/entrypoint.sh
