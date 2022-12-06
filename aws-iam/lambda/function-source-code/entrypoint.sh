eea168064624552c7ce5f68cb92c86888f7d9d80
Assaf Hazan <assafjh@gmail.com>
2022-12-06 10:43:07 +0200

    Adding missing cleanup to entrypoint

--- aws-iam/lambda/function-source-code/entrypoint.sh
+++ aws-iam/lambda/function-source-code/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 [ -f ./conjur-lambda-package.zip ] && rm -rf ./conjur-lambda-package.zip
+[ -d ./package ] && rm -rf ./package
 yum install zip -y
 pip3 install -r requirements.txt --target ./package
 cd ./package || exit
