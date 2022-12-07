69f922a9bd6958ab0de7310ee21908271dd19601
Assaf Hazan <assafjh@gmail.com>
2022-12-07 14:48:17 +0200

    Updates to policies

--- terraform/scripts/06-run-summon-plan.sh
+++ terraform/scripts/06-run-summon-plan.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # This script will run the Terraform plan summon.tf located under the plans/summon folder and prints the output as json file.
 # Before running this, please make sure to edit .env with with the relevant conjur provider attributes.
-#============ Script ===============
+#============ Variables ===============
 source .env
+# Conjur identity
+export CONJUR_AUTHN_LOGIN=$CONJUR_IDENTITY
+# Conjur identity api key
+export CONJUR_AUTHN_API_KEY=$CONJUR_API_KEY
+#============ Script ===============
 cd ../plans/summon || exit 1
 printf "===== Running speculative execution plan =====\n\n"
 terraform init
