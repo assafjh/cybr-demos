b0aee6ac0b37aee8bb67de6c9d3ebf86cfb01b07
Assaf Hazan <assafjh@gmail.com>
2022-12-20 14:01:45 +0200

    Uploading GitLab CI working demo

--- gitlab-ci/scripts/01-deploy-gitlab-server.sh
+++ gitlab-ci/scripts/01-deploy-gitlab-server.sh
@@ -5,24 +5,21 @@ SUDO=
 # Using docker/podman
 CONTAINER_MGR=docker
 # Docker image URL
-CONTAINER_IMG=gitlab/gitlab-ee:latest
-# GitLab URL (usually, hostname)
-GITLAB_ADDRESS=$(hostname -f)
+CONTAINER_IMG=gitlab/gitlab-ce:latest
+# GitLab URL (if available, use the external hostname)
+GITLAB_ADDRESS=
 # GitLab HTTP port
 GITLAB_HTTP_PORT=9080
-# GitLab SSH port
-GITLAB_SSH_PORT=9022
 # GitLab admin user password
 GITLAB_ROOT_PASSWORD=
 #============ Script ===============
 #Deploying GitLab
 $SUDO $CONTAINER_MGR run --detach \
   --hostname "$GITLAB_ADDRESS" \
-  --publish "$GITLAB_HTTP_PORT":80 \
-  --publish "$GITLAB_SSH_PORT":22 \
+  --publish "$GITLAB_HTTP_PORT":$GITLAB_HTTP_PORT \
   --name gitlab-server \
   --restart always \
   --shm-size 256m \
   --env GITLAB_ROOT_PASSWORD="$GITLAB_ROOT_PASSWORD" \
+  --env GITLAB_OMNIBUS_CONFIG="external_url 'http://$GITLAB_ADDRESS:$GITLAB_HTTP_PORT/';" \
   "$CONTAINER_IMG"
-
