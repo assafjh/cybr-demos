1361c3bfec0009a0f0513dc306b32069522da4f3
Assaf Hazan <assafjh@gmail.com>
2023-01-30 21:58:31 +0200

    updating cert follower, adding jwt follower

---
+++ kubernetes-follower/jwt-based/scripts/02_populate_variables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#============ Variables ===============
+# Using kubectl/oc
+COP_CLI=kubectl
+#============ Script ===============
+
+# Checking if a user is logged-in to Conjur-CLI
+conjur whoami
+
+# Populate authenticator values
+conjur variable set -i conjur/authn-jwt/k8s-follower1/identity-path -v "/kubernetes/followers"
+conjur variable set -i conjur/authn-jwt/k8s-follower1/issuer -v "$(echo $($COP_CLI get --raw /.well-known/openid-configuration | awk -F "," '{print $1}' | tr -d '",' | sed 's#{issuer:##g'))"
+conjur variable set -i conjur/authn-jwt/k8s-follower1/token-app-property -v "sub"
+
+# AWS EKS
+# Well Known configuration link:
+# https://oidc.eks.$REGION.amazonaws.com/id/$ID/.well-known/openid-configuration
+# conjur variable set -i conjur/authn-jwt/k8s-follower1/public-keys -v "$(echo '{"type": "jwks","value":'$(curl -s https://oidc.eks.$REGION.amazonaws.com/id/$ID/keys)}'')"
+# conjur variable set -i conjur/authn-jwt/k8s-follower1/jwks-uri -v "https://oidc.eks.$REGION.amazonaws.com/id/$ID/keys"
+
+# Other Kubernetes platforms
+conjur variable set -i conjur/authn-jwt/k8s-follower1/public-keys -v "$(echo '{"type": "jwks","value":'$($COP_CLI get --raw /openid/v1/jwks)'}')"
+
