f54b1d2142c77fec973b1f2d64e5f2ef30336f28
Assaf Hazan <assafjh@gmail.com>
2023-02-01 16:26:52 +0200

    Adding followers

--- custom-certificates/conjur/04-load-follower-certificate-to-leader.sh
+++ custom-certificates/conjur/04-load-follower-certificate-to-leader.sh
@@ -4,5 +4,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "$SCRIPT_DIR"/.env
 #============ Script ===============
-FILES_TO_COPY=("$ROOT_CA_CERTIFICATE_PATH" "$LEADER_KEY_FILE_PATH" "$LEADER_CERTIFICATE_FILE_NAME")
+FILES_TO_COPY=("$FOLLOWER_KEY_FILE_PATH" "$FOLLOWER_CERTIFICATE_FILE_PATH")
 COPY_CERTS_TO_LEADER "${FILES_TO_COPY[@]}"
+
+$SUDO $CONTAINER_MGR exec $CONTAINER_ID evoke ca import --key /certs/$(basename $LEADER_KEY_FILE_PATH) /certs/$(basename $FOLLOWER_CERTIFICATE_FILE_PATH)
