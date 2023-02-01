eae9d34ce4a793782569bf13b12477ebf9d50682
Assaf Hazan <assafjh@gmail.com>
2023-02-01 13:16:29 +0200

    Adding first draft for custom certificates

---
+++ custom-certificates/tools/02-create-self-signed-certificate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script will generate a certificate for usage with your application
+# Make sure to edit .env before running this script
+#================ Internal ==============
+source .env
+#================ Script ==============
+cd "$SCRIPT_DIR"/../certs || exit 1
+openssl genrsa -out "$LEAF_PRIVATE_CERTIFICATE_FILE_NAME" 2048
+cat > csr.conf <<EOF
+[ req ]
+default_bits = 2048
+prompt = no
+default_md = sha256
+distinguished_name = dn
+
+[ dn ]
+C = IL
+ST = Hamerkaz
+L = Tel-Aviv
+O = Org
+OU = DevSecOps
+CN = ${LEAF_CN}
+
+EOF
