181894d353f5c0e6480a87428a17810a835f1d7b
Assaf Hazan <assafjh@gmail.com>
2023-02-01 15:23:27 +0200

    added bypasses for certificate generation + conjur

--- custom-certificates/tools/02-create-self-signed-certificate.sh
+++ custom-certificates/tools/02-create-self-signed-certificate.sh
@@ -2,10 +2,15 @@
 # This script will generate a certificate for usage with your application
 # Make sure to edit .env before running this script
 #================ Internal ==============
-source .env
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) 
+source $SCRIPT_DIR/.env
 #================ Script ==============
 cd "$SCRIPT_DIR"/../certs || exit 1
-openssl genrsa -out "$SERVER_KEY_FILE_NAME" 2048
+
+[ -n "$BYPASS_SERVER_KEY_FILE_PATH" ] && SERVER_KEY_FILE_PATH="$BYPASS_SERVER_KEY_FILE_PATH"
+[ -n "$BYPASS_SERVER_CN" ] && SERVER_CN="$BYPASS_SERVER_CN"
+
+openssl genrsa -out "$SERVER_KEY_FILE_PATH" 2048
 cat > csr.conf <<EOF
 [ req ]
 default_bits = 2048
