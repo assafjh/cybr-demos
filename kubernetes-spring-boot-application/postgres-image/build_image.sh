e828aea57169296a89c6fc904ac9072ccebdf9cf
Assaf Hazan <assafjh@gmail.com>
2023-02-19 18:03:51 +0200

    Adding spring boot with postgres k8s push to file

---
+++ kubernetes-spring-boot-application/postgres-image/build_image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#=======
+CONTAINER_MGR=docker
+#=======
+"$CONTAINER_MGR" build --no-cache --tag docker.io/assafhazan/postgres-zoo-demo:v1 -f ./Dockerfile .
+"$CONTAINER_MGR" login docker.io -u assafhazan
+"$CONTAINER_MGR" push docker.io/assafhazan/postgres-zoo-demo:v1
