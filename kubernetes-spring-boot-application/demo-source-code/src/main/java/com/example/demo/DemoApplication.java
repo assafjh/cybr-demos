eccfde72b7d86845f3356f11cd8cff028d4a170b
Assaf Hazan <assafjh@gmail.com>
2023-02-20 10:14:36 +0200

    removing unused lib

--- kubernetes-spring-boot-application/demo-source-code/src/main/java/com/example/demo/DemoApplication.java
+++ kubernetes-spring-boot-application/demo-source-code/src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -8,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@Slf4j
 @PropertySource(value = "file:${user.dir}/config.properties", ignoreResourceNotFound = true)
 @PropertySource(value = "file:${user.dir}/database-configuration.properties", ignoreResourceNotFound = true)
 @PropertySource(value = "file:/conjur/secrets/database-configuration.properties", ignoreResourceNotFound = true)
