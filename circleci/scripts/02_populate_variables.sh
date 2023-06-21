85b52de9ca7c5c157059cd88ee3a8c7aac6aad56
Assaf Hazan <assafjh@gmail.com>
2023-06-21 10:09:38 +0300

    add cc support for circle ci integration

--- circleci/scripts/02_populate_variables.sh
+++ circleci/scripts/02_populate_variables.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 #============ Variables ===============
 # Path to our safe at Conjur, leave as is
-SAFE_PATH=circleci/apps/safe/secret
+SAFE_PATH=data/circleci/apps/safe/secret
 # CircleCI Organization ID
 CIRCLECI_ORG_ID=
+# If needed, modify the below to configure Conjur CLI location
+CONJUR_CLI=conjur
 #============ Script ===============
 
 # Checking if a user is logged-in to Conjur-CLI
-conjur whoami
+"$CONJUR_CLI" whoami
 
 # Populate safe secrets with values
 for i in {1..3}
 do
    if command -p md5sum  /dev/null >/dev/null 2>&1
     then
-        conjur variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
+        "$CONJUR_CLI" variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
     else
-        conjur variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
+        "$CONJUR_CLI" variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
     fi
 done
 
 # Populate authenticator values
-conjur variable set -i conjur/authn-jwt/circleci1/issuer -v "https://oidc.circleci.com/org/$CIRCLECI_ORG_ID"
-conjur variable set -i conjur/authn-jwt/circleci1/jwks-uri -v "https://oidc.circleci.com/org/$CIRCLECI_ORG_ID/.well-known/jwks-pub.json"
-conjur variable set -i conjur/authn-jwt/circleci1/token-app-property -v "sub"
-conjur variable set -i conjur/authn-jwt/circleci1/audience -v "$CIRCLECI_ORG_ID"
-conjur variable set -i conjur/authn-jwt/circleci1/identity-path -v "/circleci/apps"
+"$CONJUR_CLI" variable set -i conjur/authn-jwt/circleci1/issuer -v "https://oidc.circleci.com/org/$CIRCLECI_ORG_ID"
+"$CONJUR_CLI" variable set -i conjur/authn-jwt/circleci1/jwks-uri -v "https://oidc.circleci.com/org/$CIRCLECI_ORG_ID/.well-known/jwks-pub.json"
+"$CONJUR_CLI" variable set -i conjur/authn-jwt/circleci1/token-app-property -v "sub"
+"$CONJUR_CLI" variable set -i conjur/authn-jwt/circleci1/audience -v "$CIRCLECI_ORG_ID"
+"$CONJUR_CLI" variable set -i conjur/authn-jwt/circleci1/identity-path -v "/circleci/apps"
