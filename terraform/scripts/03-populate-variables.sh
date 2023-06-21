94e93a896c59e82efa84fa76814f374a3e68c355
Assaf Hazan <assafjh@gmail.com>
2023-06-21 09:24:49 +0300

    small fixes

--- terraform/scripts/03-populate-variables.sh
+++ terraform/scripts/03-populate-variables.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #============ Variables ===============
 # Path to our safes at Conjur, leave as is
-SAFE_PATH=terraform/plans/safe/
+SAFE_PATH=data/terraform/plans/safe/
 # Secret variable name, leave as is
 VARIABLE_NAME=secret
 # safe names, leave as is
 declare -a SAFE_NAMES=("attributes" "envvars" "summon")
-
+# If needed, modify the below to configure Conjur CLI location
+CONJUR_CLI=conjur
 #============ Script ===============
 
 # Checking if a user is logged-in to Conjur-CLI
-conjur whoami
+"$CONJUR_CLI" whoami
 
 # Populate safe secrets with values
 for i in "${SAFE_NAMES[@]}"
@@ -19,9 +20,9 @@ do
     do
         if command -p md5sum  /dev/null >/dev/null 2>&1
         then
-            conjur variable set -i "$SAFE_PATH$i/$VARIABLE_NAME$k" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
+            "$CONJUR_CLI" variable set -i "$SAFE_PATH$i/$VARIABLE_NAME$k" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
         else
-            conjur variable set -i "$SAFE_PATH$i/$VARIABLE_NAME$k" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
+            "$CONJUR_CLI" variable set -i "$SAFE_PATH$i/$VARIABLE_NAME$k" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
         fi
     done
 done
--- terraform/scripts/04-run-attributes-plan.sh
+++ terraform/scripts/04-run-attributes-plan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script will run the Terraform plan attribute.tf located under the plans/attributes folder and prints the output as json file.
-# Before running this, please make sure to edit plans/attributes/attribute.tf with with the relevant conjur provider attributes.
+# Before running this, please make sure to edit plans/attributes/attributes.tf with with the relevant conjur provider attributes.
 #============ Script ===============
 cd ../plans/attributes || exit 1
 printf "===== Running speculative execution plan =====\n\n"
