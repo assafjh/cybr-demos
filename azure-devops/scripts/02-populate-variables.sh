9226cea9deb763a519a28139264345deba207c2d
Assaf Hazan <assafjh@gmail.com>
2023-06-21 09:24:49 +0300

    small fixes

--- azure-devops/scripts/02-populate-variables.sh
+++ azure-devops/scripts/02-populate-variables.sh
@@ -3,21 +3,23 @@
 # Path to our safe at Conjur, leave as is
 SAFE_PATH=data/azure/apps/safe/secret
 AZURE_TENANT_ID=
+# If needed, modify the below to configure Conjur CLI location
+CONJUR_CLI=conjur
 #============ Script ===============
 
 # Checking if a user is logged-in to Conjur-CLI
-conjur whoami
+"$CONJUR_CLI" whoami
 
 # Populate safe secrets with values
 for i in {1..3}
 do
    if command -p md5sum  /dev/null >/dev/null 2>&1
     then
-        conjur variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
+        "$CONJUR_CLI" variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
     else
-        conjur variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
+        "$CONJUR_CLI" variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
     fi
 done
 
 # Populate authenticator values
-conjur variable set -i conjur/authn-azure/devops/provider-uri -v "https://sts.windows.net/$AZURE_TENANT_ID/"
+"$CONJUR_CLI" variable set -i conjur/authn-azure/devops/provider-uri -v "https://sts.windows.net/$AZURE_TENANT_ID/"
