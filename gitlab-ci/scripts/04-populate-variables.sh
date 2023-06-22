0e9b37b7b58a40ec7fc52803d9a17bb15e9fc993
Assaf Hazan <assafjh@gmail.com>
2023-06-22 18:13:57 +0300

    Added support for new executor,support for CC

--- gitlab-ci/scripts/04-populate-variables.sh
+++ gitlab-ci/scripts/04-populate-variables.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 #============ Variables ===============
 # Path to our safe at Conjur, leave as is
-SAFE_PATH=gitlab/ci/safe/secret
+SAFE_PATH=data/gitlab/ci/safe/secret
 # GitLab Issuer - usually GitLab server hostname
 GITLAB_ISSUER=
 # GitLab URL
 GITLAB_URL=
+# If needed, modify the below to configure Conjur CLI location
+CONJUR_CLI=conjur
 #============ Script ===============
 
 # Checking if a user is logged-in to Conjur-CLI
-conjur whoami
+"$CONJUR_CLI" whoami
 
 # Populate safe secrets with values
 for i in {1..3}
 do
    if command -p md5sum  /dev/null >/dev/null 2>&1
     then
-        conjur variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
+        "$CONJUR_CLI" variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
     else
-        conjur variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
+        "$CONJUR_CLI" variable set -i "$SAFE_PATH$i" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
     fi
 done
 
 # Populate authenticator values
-conjur variable set -i conjur/authn-jwt/gitlab1/issuer -v "$GITLAB_ISSUER"
-conjur variable set -i conjur/authn-jwt/gitlab1/jwks-uri -v "$GITLAB_URL/-/jwks/"
-conjur variable set -i conjur/authn-jwt/gitlab1/token-app-property -v "namespace_path"
-conjur variable set -i conjur/authn-jwt/gitlab1/enforced-claims -v "ref,ref_type"
-conjur variable set -i conjur/authn-jwt/gitlab1/identity-path -v "/gitlab/ci"
+"$CONJUR_CLI" variable set -i conjur/authn-jwt/gitlab1/issuer -v "$GITLAB_ISSUER"
+"$CONJUR_CLI" variable set -i conjur/authn-jwt/gitlab1/jwks-uri -v "$GITLAB_URL/-/jwks/"
+"$CONJUR_CLI" variable set -i conjur/authn-jwt/gitlab1/token-app-property -v "namespace_path"
+"$CONJUR_CLI" variable set -i conjur/authn-jwt/gitlab1/enforced-claims -v "ref,ref_type"
+"$CONJUR_CLI" variable set -i conjur/authn-jwt/gitlab1/identity-path -v "/data/gitlab/ci"
