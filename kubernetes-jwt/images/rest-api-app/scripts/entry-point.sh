0440e61fb6d500fd5ca1694da080868b275fbee2
Assaf Hazan <assafjh@gmail.com>
2023-07-09 13:25:00 +0300

    converting to images

---
+++ kubernetes-jwt/images/rest-api-app/scripts/entry-point.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "======================"
+echo "Using Service Account JWT - taken from file:"
+ls -ltr /var/run/secrets/kubernetes.io/serviceaccount/token
+echo "======================"
+./retrieve.sh
+sleep infinity
---
+++ kubernetes-jwt/images/rest-api-app/scripts/retrieve.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Using Service Account JWT
+JWT=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+# Authenticating to Conjur using above JWT
+token=$(curl -k -s --request POST "$CONJUR_AUTHN_URL/$CONJUR_ACCOUNT/authenticate" --header 'Content-Type: application/x-www-form-urlencoded' --header "Accept-Encoding: base64" --data-urlencode "jwt=$JWT")
+# Retrieving secret using short lived access token revcieved from Conjur
+secret=$(curl -k -s -X GET -H "Authorization: Token token=\"$token\"" "$CONJUR_APPLIANCE_URL/secrets/$CONJUR_ACCOUNT/variable/$CONJUR_VARIABLE_PATH")
+# Printing the secret   
+echo "$CONJUR_VARIABLE_PATH value: $secret"
