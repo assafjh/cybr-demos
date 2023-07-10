6bc8f30abc7a02572a5446772c0bc7345d9e4ebf
Assaf Hazan <assafjh@gmail.com>
2023-07-10 11:29:33 +0300

    cleanup

--- kubernetes-secretless/images/app-image/build_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#=======
-CONTAINER_MGR=docker
-#=======
-"$CONTAINER_MGR" build --no-cache --tag docker.io/assafhazan/postgres-secretless-demo:v1 -f ./Dockerfile .
-"$CONTAINER_MGR" login docker.io -u assafhazan
-"$CONTAINER_MGR" push docker.io/assafhazan/postgres-secretless-demo:v1
--- kubernetes-secretless/images/postgres-image/02-build-image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#=======
-CONTAINER_MGR=docker
-#=======
-"$CONTAINER_MGR" build --no-cache --tag docker.io/assafhazan/postgres-zoo-demo:v11.2 -f ./Dockerfile .
-"$CONTAINER_MGR" login docker.io -u assafhazan
-"$CONTAINER_MGR" push docker.io/assafhazan/postgres-zoo-demo:v11.2
