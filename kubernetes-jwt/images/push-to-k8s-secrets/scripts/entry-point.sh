d5d08a1e480df33aff4783913c2d05242d8ef284
Assaf Hazan <assafjh@gmail.com>
2023-07-11 12:52:45 +0300

    unified spring boot with k8s uc, refactoring

--- kubernetes-jwt/images/push-to-k8s-secrets/scripts/entry-point.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "======================"
-echo "env output:"
-env
-echo "======================"
-echo "env | grep SECRET output:"
-env | grep SECRET
-sleep infinity
--- kubernetes-jwt/images/summon/README.md
+++
@@ -1,3 +0,0 @@
-# Summon Demo Image
-This is example Dockerfile describes the demo image which I use in the demo.
-The image is based on alpine.
