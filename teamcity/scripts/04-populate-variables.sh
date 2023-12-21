fc7b1c9e2164f6aa03ba243c8411f6ecf4a4fb90
Assaf Hazan <assafjh@gmail.com>
2023-12-21 15:21:34 +0000

    Adding uc for teamcity

---
+++ teamcity/scripts/04-populate-variables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#============ Variables ===============
+# Path to our safe at Conjur, leave as is
+SAFE_PATH=data/teamcity/projects/safe/project1
+# Secret variable name, leave as is
+VARIABLE_NAME=secret
+# If needed, modify the below to configure Conjur CLI location
+CONJUR_CLI=conjur
+#============ Script ===============
+
+# Checking if a user is logged-in to Conjur-CLI
+"$CONJUR_CLI" whoami
+
+# Populate safe secrets with values
+for i in {1..3}
+do
+   if command -p md5sum  /dev/null >/dev/null 2>&1
+    then
+        "$CONJUR_CLI" variable set -i "${SAFE_PATH}/${VARIABLE_NAME}$i" -v "$(echo $RANDOM | md5sum | head -c 20; echo;)"
+    else
+        "$CONJUR_CLI" variable set -i "${SAFE_PATH}/${VARIABLE_NAME}$i" -v "$(echo $RANDOM | md5 | head -c 20; echo;)"
+    fi
+done
