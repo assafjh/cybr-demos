a19a50ec7787a7b78167726d286cdc518d853d30
Assaf Hazan <assafjh@gmail.com>
2024-06-03 12:40:07 +0300

    jakarta

--- application-server-credential-provider/code/demo-app/src/main/java/com/example/ZooServlet.java
+++ application-server-credential-provider/code/demo-app/src/main/java/com/example/ZooServlet.java
@@ -5,14 +5,14 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
+import jakarta.naming.InitialContext;
+import jakarta.naming.NamingException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.sql.DataSource;
 
 @WebServlet("/zoo")
 public class ZooServlet extends HttpServlet {
