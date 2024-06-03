bfcc63ac029530f706d0668d52406ea67fa937e0
Assaf Hazan <assafjh@gmail.com>
2024-06-03 11:21:37 +0000

    Adding CP DS and code support

--- application-server-credential-provider/scripts/04-configure-datasource.sh
+++ application-server-credential-provider/scripts/04-configure-datasource.sh
@@ -3,14 +3,26 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Variables
+# -- Variables --
+# -- Tomcat --
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TOMCAT_HOME="$SCRIPT_DIR/tomcat"
+CONTEXT_XML="$TOMCAT_HOME/conf/context.xml"
+# -- Postgres --
 POSTGRES_JDBC_URL="https://jdbc.postgresql.org/download/postgresql-42.2.24.jar"
 POSTGRES_JDBC_JAR="postgresql-42.2.24.jar"
-CONTEXT_XML="$TOMCAT_HOME/conf/context.xml"
-DB_HOST=$(hostname)
-DB_PORT=5433
+POSTGRES_DB_HOST=$(hostname)
+POSTGRES_DB_PORT=5433
+POSTGRES_DB_NAME=vet
+POSTGRES_DB_USER=reception
+POSTGRES_DB_PASSWORD=vet_123456
+# -- Credential Provider --
+CYBERARK_JDBC_JAR="/opt/CARKaim/sdk/javapasswordsdk.jar"
+APP_ID=AIMWebService
+SAFE=Demo-Safe-General
+OBJECT=
+FOLDER=Root
+REASON=Demo
 
 # Ensure the script is run as root or with sudo if needed
 SUDO=
@@ -23,24 +35,46 @@ wget -q $POSTGRES_JDBC_URL -O /tmp/$POSTGRES_JDBC_JAR
 echo "Copying PostgreSQL JDBC Driver to Tomcat's lib directory..."
 $SUDO cp /tmp/$POSTGRES_JDBC_JAR $TOMCAT_HOME/lib/
 
+# Copy CyberArk JDBC Driver to Tomcat's lib directory
+echo "Copying CyberArk JDBC Driver to Tomcat's lib directory..."
+$SUDO cp $CYBERARK_JDBC_JAR $TOMCAT_HOME/lib/
+
 # Configure DataSource in context.xml
 echo "Configuring DataSource in context.xml..."
 if grep -q "<Resource name=\"jdbc/PostgresDS\"" "$CONTEXT_XML"; then
-    echo "DataSource already configured in context.xml"
+    echo "Postgres DataSource already configured in context.xml"
 else
     $SUDO sed -i "/<\/Context>/i \
     <Resource name=\"jdbc/PostgresDS\" \
               auth=\"Container\" \
               type=\"javax.sql.DataSource\" \
-              username=\"reception\" \
-              password=\"vet_123456\" \
+              username=\"$POSTGRES_DB_USER\" \
+              password=\"$POSTGRES_DB_PASSWORD\" \
               driverClassName=\"org.postgresql.Driver\" \
-              url=\"jdbc:postgresql://$DB_HOST:$DB_PORT/vet\" \
-              maxActive=\"20\" \
+              url=\"jdbc:postgresql://$POSTGRES_DB_HOST:$POSTGRES_DB_PORT/$POSTGRES_DB_NAME\" \
+              maxTotal=\"20\" \
               maxIdle=\"10\" \
-              maxWait=\"-1\"/>" "$CONTEXT_XML"
-    echo "DataSource configuration added to context.xml"
+              maxWaitMillis=\"-1\"/>" "$CONTEXT_XML"
+    echo "Postgres DataSource configuration added to context.xml"
 fi
 
-echo "PostgreSQL DataSource installation completed."
+if grep -q "<Resource name=\"jdbc/CyberArkDS\"" "$CONTEXT_XML"; then
+    echo "CyberArk DataSource already configured in context.xml"
+else
+    $SUDO sed -i "/<\/Context>/i \
+    <Resource name=\"jdbc/CyberArkDS\" \
+              auth=\"Container\" \
+              type=\"javax.sql.DataSource\" \
+              driverClassName=\"com.cyberark.jdbc.ASCPDriver\" \
+              url=\"jdbc:ascp:jdbc:postgresql://$POSTGRES_DB_HOST:$POSTGRES_DB_PORT/$POSTGRES_DB_NAME\" \
+              username=\"dummy\" \
+              password=\"dummy\" \
+              connectionProperties=\" \
+              ascp_AppId=$APP_ID; \
+              ascp_Query=Safe=$SAFE,Folder=$FOLDER,Object=$OBJCET,Reason=$REASON; \
+              ascp_VendorClass=org.postgresql.Driver; >" "$CONTEXT_XML"
+    echo "CyberArk DataSource configuration added to context.xml"
+fi
+
+echo "PostgreSQL and CyberArk DataSource installation completed."
 
