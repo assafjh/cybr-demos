d33f9c0d972c2b94651e86d552db2104651b448b
Assaf Hazan <assafjh@gmail.com>
2024-06-19 19:00:51 +0300

    fix

--- kubernetes-jwt-priv-cloud/ansible/02-run-book.sh
+++ kubernetes-jwt-priv-cloud/ansible/02-run-book.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set environment variables
-export CLIENT_ID="assaf-sa@assaflab"
-export CLIENT_SECRET="SomePass123@"
+export CLIENT_ID=""
+export CLIENT_SECRET=""
 
 # Run the Ansible playbook
 ansible-playbook safe-onboarding.yml
