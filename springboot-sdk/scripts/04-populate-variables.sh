5ba25867cb8f9ee6a1128ac0b1c95a410fc818cb
Assaf Hazan <assafjh@gmail.com>
2025-01-08 17:36:13 +0200

    update

--- springboot-sdk/scripts/04-populate-variables.sh
+++ springboot-sdk/scripts/04-populate-variables.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 #============ Variables ===============
+# Script path
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # If needed, modify the below to configure Conjur CLI location
 CONJUR_CLI=/Applications/ConjurCloudCLI.app/Contents/Resources/conjur/conjur
+
+# JWKS location
+JWKS_FILE="$SCRIPT_DIR/../compiled/jwks.json"
 
 #============ Script ===============
 
@@ -12,4 +18,22 @@ CONJUR_CLI=/Applications/ConjurCloudCLI.app/Contents/Resources/conjur/conjur
 "$CONJUR_CLI" variable set -i conjur/authn-jwt/springboot1/identity-path -v "/data/springboot"
 "$CONJUR_CLI" variable set -i conjur/authn-jwt/springboot1/issuer -v "springboot-conjur-demo-app"
 "$CONJUR_CLI" variable set -i conjur/authn-jwt/springboot1/token-app-property -v "sub"
-"$CONJUR_CLI" variable set -i conjur/authn-jwt/springboot1/jwks-uri -v "http://18.175.89.69:9090/.well-known/jwks.json"
+
+#============ JWKS ===============
+
+# Check if the JWKS file exists
+if [ ! -f "$JWKS_FILE" ]; then
+    echo "Error: JWKS file not found at $JWKS_FILE"
+    exit 1
+fi
+
+# Extract the JWKS key entry (entire "keys" array)
+JWKS_ENTRY=$(jq '.keys[0]' "$JWKS_FILE")
+
+# Check if the key entry is found
+if [ -z "$JWKS_ENTRY" ]; then
+    echo "Error: No valid key entry found in JWKS file."
+    exit 1
+fi
+
+"$CONJUR_CLI" variable set -i conjur/authn-jwt/springboot1/public-keys -v "$JWKS_ENTRY"
