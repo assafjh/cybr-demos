8f60915e6f69d5ecb93d31de1d4012140fd7c1c2
Assaf Hazan <assafjh@gmail.com>
2025-01-08 18:13:05 +0200

    fixes

--- springboot-sdk/code/demo/src/main/java/com/cyberark/conjur/demo/config/AppConfig.java
+++ springboot-sdk/code/demo/src/main/java/com/cyberark/conjur/demo/config/AppConfig.java
@@ -14,19 +14,10 @@ public class AppConfig {
     private String applianceUrl;
     private AuthnJwt authnJwt;
     private Map<String, String> secrets;
-<<<<<<< HEAD
     private String jwksFilePath; // Path to JWKS file
-=======
->>>>>>> d98950cfea16f019d4c8dfb991cf693ba52ac46c
 
     @Data
     public static class AuthnJwt {
         private String serviceId;
     }
-<<<<<<< HEAD
-=======
-
-    // JWKS file path property
-    private String jwksFilePath;
->>>>>>> d98950cfea16f019d4c8dfb991cf693ba52ac46c
 }
