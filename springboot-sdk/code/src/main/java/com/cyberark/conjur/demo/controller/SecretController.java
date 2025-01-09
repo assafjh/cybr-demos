25d6110952e3e0bed0c95c63a9d2cd7f75c59f66
Assaf Hazan <assafjh@gmail.com>
2025-01-09 14:19:59 +0200

    complete refactor - version works with host and key

--- springboot-sdk/code/src/main/java/com/cyberark/conjur/demo/controller/SecretController.java
+++ springboot-sdk/code/src/main/java/com/cyberark/conjur/demo/controller/SecretController.java
@@ -1,61 +1,33 @@
 package com.cyberark.conjur.demo.controller;
 
-import com.cyberark.conjur.demo.config.AppConfig;
-import com.cyberark.conjur.demo.util.JwtUtil;
-import com.cyberark.conjur.springboot.annotations.ConjurPropertySource;
-import com.cyberark.conjur.springboot.annotations.ConjurValues;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import com.cyberark.conjur.demo.config.DynamicSecretConfiguration;
 
 @RestController
-@RequiredArgsConstructor
-@Slf4j
-@ConjurPropertySource({
-    "${conjur.secrets.my-secret1}",
-    "${conjur.secrets.my-secret2}",
-    "${conjur.secrets.my-secret3}"
-})
 public class SecretController {
 
-    private final AppConfig appConfig;
+    private final DynamicSecretConfiguration dynamicSecretConfiguration;
 
-    @ConjurValues(keys={
-        "${conjur.secrets.my-secret1}",
-        "${conjur.secrets.my-secret2}",
-        "${conjur.secrets.my-secret3}"
-    })
-    
-    private Map<String, String> secrets;
-
-    @GetMapping("/secrets")
-    public Map<String, String> getSecrets() {
-        log.info("Starting secrets retrieval process");
-
-        // Check for 'init' mode
-        String jwksFilePath = appConfig.getJwksFilePath();
-        String initMode = System.getProperty("mode");
-        if ("init".equalsIgnoreCase(initMode)) {
-            try {
-                JwtUtil.generateAndSaveJWKS(jwksFilePath);
-                log.info("JWKS generated and saved at {}", jwksFilePath);
-                return Map.of("message", "JWKS generated successfully!");
-            } catch (Exception e) {
-                log.error("Error generating JWKS: ", e);
-                throw new RuntimeException("Error occurred while generating JWKS.", e);
-            }
-        }
-
-        try {
-            // Retrieve and log secrets
-            secrets.forEach((key, value) -> log.info("Retrieved secret: {} = {}", key, value));
-            return secrets;
-        } catch (Exception e) {
-            log.error("Error while retrieving secrets: ", e);
-            throw new RuntimeException("Error occurred while retrieving the secrets.", e);
-        }
+    public SecretController(DynamicSecretConfiguration dynamicSecretConfiguration) {
+        this.dynamicSecretConfiguration = dynamicSecretConfiguration;
     }
+
+    @GetMapping(value = "/secret", produces = MediaType.TEXT_HTML_VALUE)
+    public String getSecret() {
+        return String.format(
+            "Retrieved <b>%s</b>: %s<br>" +
+            "Retrieved <b>%s</b>: %s<br>" +
+            "Retrieved <b>%s</b>: %s",
+            dynamicSecretConfiguration.getSecretOne(),
+            dynamicSecretConfiguration.getSecretOneValue(),
+            dynamicSecretConfiguration.getSecretTwo(),
+            dynamicSecretConfiguration.getSecretTwoValue(),
+            dynamicSecretConfiguration.getSecretThree(),
+            dynamicSecretConfiguration.getSecretThreeValue()
+        );
+    }
+    
 }
