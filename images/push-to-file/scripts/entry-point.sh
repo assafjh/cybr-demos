c87e24a6f4e0167f1f0a6dc1bf3aca1d11a4d318
Assaf Hazan <assafjh@gmail.com>
2026-05-03 15:10:25 +0300

    migrating push to file img

--- images/push-to-file/scripts/entry-point.sh
+++ images/push-to-file/scripts/entry-point.sh
@@ -1,32 +1,38 @@
 #!/bin/sh
 
-ELASPED=0
+ELAPSED=0
+
+echo "[INFO] Starting App Container - Waiting for Conjur Secrets Provider..."
 
 while [ ! -s "$INJECTED_FILES_PATH/credentials.yaml" ] || [ ! -s "$INJECTED_FILES_PATH/credentials.properties" ]; do
-  echo "[INFO] Waiting for Conjur secrets to be injected..."
   sleep 1
-  ELASPED=$((ELASPED + 1))
-  if [ "$ELASPED" -ge "$WAIT_TIMEOUT" ]; then
-    echo "Timed out waiting for secrets. Exiting."
+  ELAPSED=$((ELAPSED + 1))
+  if [ "$ELAPSED" -ge "$WAIT_TIMEOUT" ]; then
+    echo "[ERROR] Timed out ($WAIT_TIMEOUT sec) waiting for secrets. Exiting."
     exit 1
   fi
 done
 
-echo "======================"
-echo "Files were injected by sidecar:"
+echo "=========================================="
+echo "✅ Secrets successfully injected by Sidecar:"
 ls -ltr "$INJECTED_FILES_PATH/"
-echo "======================"
-echo "printing $INJECTED_FILES_PATH/credentials.yaml"
-cat "$INJECTED_FILES_PATH/credentials.properties"
+echo "=========================================="
+
 echo ""
-echo "======================"
-echo "printing $INJECTED_FILES_PATH/credentials.properties"
+echo "[CONTENT] -> $INJECTED_FILES_PATH/credentials.yaml"
+cat "$INJECTED_FILES_PATH/credentials.yaml"
+
+echo ""
+echo "[CONTENT] -> $INJECTED_FILES_PATH/credentials.properties"
 cat "$INJECTED_FILES_PATH/credentials.properties"
+echo "=========================================="
+
 if [ "$RUN_MESSENGER" = "true" ]; then
     echo ""
-    echo "======================"
-    echo "running $INJECTED_FILES_PATH/messenger"
+    echo "[INFO] Running Messenger App: $INJECTED_FILES_PATH/messenger"
     chmod +x "$INJECTED_FILES_PATH/messenger"
     "$INJECTED_FILES_PATH/messenger"
 fi
+
+echo "[INFO] App initialization complete. Sleeping infinity..."
 sleep infinity
