b9e8764bf61351e61fedb5ca75227fbd2157049c
Assaf Hazan <assafjh@gmail.com>
2026-05-18 11:26:51 +0300

    fix for amazon linux workflow

--- aws-iam/lambda/function-source-code/entrypoint.sh
+++ aws-iam/lambda/function-source-code/entrypoint.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 [ -f ./conjur-lambda-package.zip ] && rm -f ./conjur-lambda-package.zip
 [ -d ./package ] && rm -rf ./package
-yum install zip -y
+dnf install zip -y
 pip3 install -r requirements.txt --target ./package
 cd ./package || exit
 zip -r ../conjur-lambda-package.zip .
